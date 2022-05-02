4dbbc066bd1427de95d842bbbc3ec22b05e89907
ainunseptian <ainunseptian79@gmail.com>
2022-05-02 05:58:23 -0400

    create card dynamic data

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ $.ajax({
   url: "http://www.omdbapi.com/?apikey=dca61bcc&s=avengers",
   success: (results) => {
     const movies = results.Search;
-    console.log(movies);
+    let cards = "";
+    movies.forEach((movie) => {
+      cards += `<div class="col-md-4 my-3">
+                    <div class="card">
+                        <img src="${movie.Poster}" class="card-img-top" />
+                        <div class="card-body">
+                            <h5 class="card-title">${movie.Title}</h5>
+                            <h6 class="card-subtittle mb-2 text-muted">${movie.Year}</h6>
+                            <a href="#" class="btn btn-primary">Show Details</a>
+                        </div>
+                     </div>
+                </div>`;
+    });
+    $(".movie-container").html(cards);
   },
   error: (errors) => {
     const errorMessage = errors.responseText;
