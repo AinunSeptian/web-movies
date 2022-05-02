2b4ca3e32e94817faa67ed9db200def36a1381cb
ainunseptian <ainunseptian79@gmail.com>
2022-05-02 06:44:36 -0400

    fixing modal and get data details

--- script.js
+++ script.js
@@ -10,13 +10,55 @@ $.ajax({
                         <div class="card-body">
                             <h5 class="card-title">${movie.Title}</h5>
                             <h6 class="card-subtittle mb-2 text-muted">${movie.Year}</h6>
-                            <a href="#" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#movieDetailModal">Show Details</a>
+                            <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal"
+                            data-bs-target="#movieDetailModal" data-imdbid="${movie.imdbID}" >Show Details</a>
                         </div>
                      </div>
                 </div>`;
     });
     $(".movie-container").html(cards);
+
+    // ketika tombol detail modal di-klik
+    $(".modal-detail-button").on("click", function () {
+      $.ajax({
+        url:
+          "http://www.omdbapi.com/?apikey=dca61bcc&i=" + $(this).data("imdbid"),
+        success: (movie) => {
+          const movieDetail = ` <div class="container-fluid">
+                                    <div class="row">
+                                    <div class="col-md-3">
+                                        <img src=${movie.Poster} class="img-fluid" />
+                                    </div>
+                                    <div class="col-md">
+                                        <ul class="list-group">
+                                        <li class="list-group-item"><h4>${movie.Title} (${movie.Year})</h4></li>
+                                        <li class="list-group-item">
+                                            <strong>Director : </strong>${movie.Director}
+                                        </li>
+                                        <li class="list-group-item">
+                                            <strong>Actors : </strong>${movie.Actors}
+                                        </li>
+                                        <li class="list-group-item">
+                                            <strong>Writer : </strong>${movie.Writer}
+                                        </li>
+                                        <li class="list-group-item">
+                                            <strong>Plot : </strong> <br />
+                                            ${movie.Plot}
+                                        </li>
+                                        </ul>
+                                    </div>
+                                    </div>
+                                </div>`;
+
+          $(".modal-body").html(movieDetail);
+        },
+        error: (errors) => {
+          const errorMessage = errors.responseText;
+          alert(errorMessage);
+          console.log(errorMessage);
+        },
+      });
+    });
   },
   error: (errors) => {
     const errorMessage = errors.responseText;
