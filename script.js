62d80312cc042adf8ddcac862881514bf942b3ac
ainunseptian <ainunseptian79@gmail.com>
2022-05-02 07:23:52 -0400

    fixing clean script

--- script.js
+++ script.js
@@ -4,17 +4,7 @@ $.ajax({
     const movies = results.Search;
     let cards = "";
     movies.forEach((movie) => {
-      cards += `<div class="col-md-4 my-3">
-                    <div class="card">
-                        <img src="${movie.Poster}" class="card-img-top" />
-                        <div class="card-body">
-                            <h5 class="card-title">${movie.Title}</h5>
-                            <h6 class="card-subtittle mb-2 text-muted">${movie.Year}</h6>
-                            <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal"
-                            data-bs-target="#movieDetailModal" data-imdbid="${movie.imdbID}" >Show Details</a>
-                        </div>
-                     </div>
-                </div>`;
+      cards += showCards(movie);
     });
     $(".movie-container").html(cards);
 
@@ -24,31 +14,7 @@ $.ajax({
         url:
           "http://www.omdbapi.com/?apikey=dca61bcc&i=" + $(this).data("imdbid"),
         success: (movie) => {
-          const movieDetail = ` <div class="container-fluid">
-                                    <div class="row">
-                                    <div class="col-md-3">
-                                        <img src=${movie.Poster} class="img-fluid" />
-                                    </div>
-                                    <div class="col-md">
-                                        <ul class="list-group">
-                                        <li class="list-group-item"><h4>${movie.Title} (${movie.Year})</h4></li>
-                                        <li class="list-group-item">
-                                            <strong>Director : </strong>${movie.Director}
-                                        </li>
-                                        <li class="list-group-item">
-                                            <strong>Actors : </strong>${movie.Actors}
-                                        </li>
-                                        <li class="list-group-item">
-                                            <strong>Writer : </strong>${movie.Writer}
-                                        </li>
-                                        <li class="list-group-item">
-                                            <strong>Plot : </strong> <br />
-                                            ${movie.Plot}
-                                        </li>
-                                        </ul>
-                                    </div>
-                                    </div>
-                                </div>`;
+          const movieDetail = showMovieDetail(movie);
 
           $(".modal-body").html(movieDetail);
         },
@@ -66,3 +32,45 @@ $.ajax({
     console.log(errorMessage);
   },
 });
+
+const showCards = (movie) => {
+  return `<div class="col-md-4 my-3">
+            <div class="card">
+                <img src="${movie.Poster}" class="card-img-top" />
+                <div class="card-body">
+                    <h5 class="card-title">${movie.Title}</h5>
+                    <h6 class="card-subtittle mb-2 text-muted">${movie.Year}</h6>
+                    <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal"
+                    data-bs-target="#movieDetailModal" data-imdbid="${movie.imdbID}" >Show Details</a>
+                </div>
+            </div>
+        </div>`;
+};
+
+const showMovieDetail = (movie) => {
+  return ` <div class="container-fluid">
+                <div class="row">
+                <div class="col-md-3">
+                    <img src=${movie.Poster} class="img-fluid" />
+                </div>
+                <div class="col-md">
+                    <ul class="list-group">
+                    <li class="list-group-item"><h4>${movie.Title} (${movie.Year})</h4></li>
+                    <li class="list-group-item">
+                        <strong>Director : </strong>${movie.Director}
+                    </li>
+                    <li class="list-group-item">
+                        <strong>Actors : </strong>${movie.Actors}
+                    </li>
+                    <li class="list-group-item">
+                        <strong>Writer : </strong>${movie.Writer}
+                    </li>
+                    <li class="list-group-item">
+                        <strong>Plot : </strong> <br />
+                        ${movie.Plot}
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </div>`;
+};
