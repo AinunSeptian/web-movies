d8973a0d4b76edf3dde2e7a54daf729088ca2820
ainunseptian <ainunseptian79@gmail.com>
2022-05-08 03:55:53 -0400

    error handling

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", async function () {
-  const inputKeyword = document.querySelector(".input-keyword");
-  const movies = await getMovies(inputKeyword.value);
-  updateUI(movies);
+  try {
+    const inputKeyword = document.querySelector(".input-keyword");
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies);
+  } catch (err) {
+    alert(err);
+  }
 });
+
+function getMovies(keyword) {
+  return fetch("http://www.omdbapi.com/?apikey=dca61bcc&s=" + keyword)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
+    .then((res) => {
+      if (res.Response === "False") {
+        throw new Error(res.Error);
+      }
+      return res.Search;
+    });
+}
+
+function updateUI(movies) {
+  let cards = "";
+  movies.forEach((movie) => (cards += showCards(movie)));
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = cards;
+}
 
 // event binding
 document.addEventListener("click", async function (e) {
-  if (e.target.classList.contains("modal-detail-button")) {
-    const imdbid = e.target.dataset.imdbid;
-    const movieDetail = await getMovieDetail(imdbid);
-    updateUIDetail(movieDetail);
+  try {
+    if (e.target.classList.contains("modal-detail-button")) {
+      const imdbid = e.target.dataset.imdbid;
+      const movieDetail = await getMovieDetail(imdbid);
+      updateUIDetail(movieDetail);
+    }
+  } catch (err) {
+    alert(err);
   }
 });
 
@@ -24,19 +55,6 @@ function updateUIDetail(movie) {
   const movieDetail = showMovieDetail(movie);
   const modalBody = document.querySelector(".modal-body");
   modalBody.innerHTML = movieDetail;
-}
-
-function getMovies(keyword) {
-  return fetch("http://www.omdbapi.com/?apikey=dca61bcc&s=" + keyword)
-    .then((res) => res.json())
-    .then((res) => res.Search);
-}
-
-function updateUI(movies) {
-  let cards = "";
-  movies.forEach((movie) => (cards += showCards(movie)));
-  const movieContainer = document.querySelector(".movie-container");
-  movieContainer.innerHTML = cards;
 }
 
 const showCards = (movie) => {
