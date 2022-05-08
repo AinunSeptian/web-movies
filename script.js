09a97397796524b2e65ed5960da3381bfad2f885
ainunseptian <ainunseptian79@gmail.com>
2022-05-08 03:28:42 -0400

    cleaning code

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// const searchButton = document.querySelector(".search-button");
-// searchButton.addEventListener("click", function () {
-//   const inputKeyword = document.querySelector(".input-keyword");
-//   fetch("http://www.omdbapi.com/?apikey=dca61bcc&s=" + inputKeyword.value)
-//     .then((res) => res.json())
-//     .then((res) => {
-//       const movies = res.Search;
-//       let cards = "";
-//       movies.forEach((movie) => (cards += showCards(movie)));
-//       const movieContainer = document.querySelector(".movie-container");
-//       movieContainer.innerHTML = cards;
-
-//       // Ketika tombol detail di-klik
-//       const modalDetailButton = document.querySelectorAll(
-//         ".modal-detail-button"
-//       );
-//       modalDetailButton.forEach((btn) => {
-//         btn.addEventListener("click", function () {
-//           const imdbid = this.dataset.imdbid;
-//           fetch("http://www.omdbapi.com/?apikey=dca61bcc&i=" + imdbid)
-//             .then((res) => res.json())
-//             .then((movie) => {
-//               const movieDetail = showMovieDetail(movie);
-//               const modalBody = document.querySelector(".modal-body");
-//               modalBody.innerHTML = movieDetail;
-//             });
-//         });
-//       });
-//     });
-// });
-
 const searchButton = document.querySelector(".search-button");
 searchButton.addEventListener("click", async function () {
   const inputKeyword = document.querySelector(".input-keyword");
